a9d4d9ac37ff31cc5f722a6b491b877a270ac553
mtisby <marieltisby1998@gmail.com>
2021-09-19 11:32:14 -0700

    structure

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-import { express } from express;
+import express, { application } from "express";
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('./home')
+})
 
 app.listen(3000, () => {
     console.log(`listening on : ${port}`)
---
+++ models/campground.js
@@ -0,0 +1,10 @@
+import { mongoose, Schema } from "mongoose"
+
+const Schmea = mongoose.Schmea;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: Number,
+    description: String,
+    location: String
+})
