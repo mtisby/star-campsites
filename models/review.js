4cd974f02fab7bb6c5b571785d12f0932c34d0fe
mtisby <marieltisby1998@gmail.com>
2021-10-06 11:06:37 -0700

    restful routes

---
+++ models/review.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    body: String,
+    rating: Number,
+})
+
+const Review = mongoose.model('Review', reviewSchema);
+
+export { Review }
