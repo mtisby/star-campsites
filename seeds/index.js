9d1b1ab61417d1d510ca2310c83abf7a2a033e11
mtisby <marieltisby1998@gmail.com>
2021-11-04 16:25:59 -0700

    deploy prep

--- seeds/index.js
+++ seeds/index.js
@@ -10,8 +10,6 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/star-campsites';
 
 mongoose.connect(dbUrl);
 
-console.log(images.length)
-
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
