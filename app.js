482d4564881c5dd14ec275c535aaa2b12840ad7a
mtisby <marieltisby1998@gmail.com>
2021-11-04 15:37:44 -0700

    deploy

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ import MongoStore from "connect-mongo"
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/star-campsites';
 
+console.log(dbUrl)
+
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
